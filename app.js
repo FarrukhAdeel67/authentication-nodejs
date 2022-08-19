c0e5ee26145a341c71e308ef7c2e3b25b82cba1a
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-08-19 02:35:45 -0700

    routes and configurations are defined

--- app.js
+++ app.js
@@ -7,12 +7,34 @@ const port = process.env.port||4000;
 const handlebars = exphbs.create({ extname: ".hbs" });
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
-app.use(express.json());
+
 //defualt option
+app.use(express.json());
 app.use(fileUpload());
+//static files 
+app.use(express.static('public'));
+app.use(express.static('upload'));
+
 app.get('',(req,res)=>{
     res.render('index');
-}),
+})
+app.post('', (req,res )=>{
+    let sampleFile;
+    let uploadPath;
+    if(!req.files||Object.keys(req.files).length ==0){
+        return res.status(400).send("no files were uploaded...")
+    }
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + '/upload/' + sampleFile.name;
+    console.log(sampleFile);
+    //user mv() funciion to place file on the server
+    sampleFile.mv(uploadPath, function (err){
+        if(err){
+            return res.status(500).send(err);
+        }
+        res.status(200).send('file uploaded...');
+    });
+})
 app.listen(port, ()=>{
     console.log(`server is running at port : ${port}`);
 })
